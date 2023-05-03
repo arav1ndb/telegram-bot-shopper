7e3d17c484826b90068d11d552dd79d672339820
Aravinda Balaji <k.aravindbalaji2001@gmail.com>
2023-05-03 21:26:49 +0530

    added url

--- bot/credentials.py
+++ bot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "6026142184:AAE1AGVwwXq5AKSx0YhM2Hiobv8kWQjAdiI"
 bot_user_name = "Aravind's shopping bot"
-URL = "the heroku app link that we will create later"
+URL = "https://telegram-shopper-bot.onrender.com"
